056f87f2cd89138f58434f2db44233ee627409d1
romer8 <gio.busrom@gmail.com>
2024-06-04 15:51:13 -0600

    change ami id. Different in each region

--- terraform/self_hosted_timescale/variables.tf
+++ terraform/self_hosted_timescale/variables.tf
@@ -6,7 +6,7 @@ variable "region" {}
 variable "db_user" {}
 variable "db_password" {}
 variable "aws_ami" {
-  default = "ami-09e647bf7a368e505"
+  default = "ami-00beae93a2d981137"
 }
 variable "private_key" {}
 variable "public_key" {}
