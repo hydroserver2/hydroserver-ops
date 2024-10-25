c574795f85f7b1e19c396c265b2910fe73137a96
Kenneth Lippold <kjlippold@outlook.com>
2024-10-25 16:48:56 -0700

    Update vpc.tf

--- terraform/gcp/network/vpc.tf
+++ terraform/gcp/network/vpc.tf
@@ -5,9 +5,6 @@
 resource "google_compute_network" "vpc_network" {
   name                    = "hydroserver-${var.instance}"
   auto_create_subnetworks = true
-  labels = {
-    "${var.label_key}" = local.label_value
-  }
 }
 
 resource "google_vpc_access_connector" "vpc_access_connector" {
@@ -15,7 +12,4 @@ resource "google_vpc_access_connector" "vpc_access_connector" {
   region        = var.region
   network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.8.0.0/28"
-  labels = {
-    "${var.label_key}" = local.label_value
-  }
 }
