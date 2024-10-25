5ae379cb0e0a60f1d49d5e97fc5c2bcd0f5e6973
Kenneth Lippold <kjlippold@outlook.com>
2024-10-25 12:57:25 -0700

    Update cloud-run.tf

--- terraform/gcp/app/cloud-run.tf
+++ terraform/gcp/app/cloud-run.tf
@@ -8,6 +8,7 @@ resource "google_cloud_run_service" "hydroserver_api" {
 
   template {
     spec {
+      service_account_name = google_service_account.cloud_run_service_account.email
       containers {
         image = "${var.region}-docker.pkg.dev/${data.google_project.gcp_project.project_id}/${var.instance}/hydroserver-api-services:latest"
         resources {
@@ -18,8 +19,30 @@ resource "google_cloud_run_service" "hydroserver_api" {
         ports {
           container_port = 8000
         }
+        env {
+          name  = "DATABASE_URL"
+          value_from {
+            secret_key_ref {
+              name = "hydroserver-db-connection-${var.instance}"
+              key  = "latest"
+            }
+          }
+        }
+        env {
+          name  = "SECRET_KEY"
+          value_from {
+            secret_key_ref {
+              name = "hydroserver-secret-key-${var.instance}"
+              key  = "latest"
+            }
+          }
+        }
       }
     }
+  }
+
+  vpc_access_connector {
+    name = var.vpc_name
   }
 
   traffic {
@@ -33,19 +56,41 @@ resource "google_cloud_run_service" "hydroserver_api" {
 }
 
 # -------------------------------------------------- #
+# HydroServer GCP Cloud Run Service Account          #
+# -------------------------------------------------- #
+
+resource "google_service_account" "cloud_run_service_account" {
+  account_id   = "hydroserver-api-${var.instance}"
+  display_name = "HydroServer Cloud Run Service Account - ${var.instance}"
+  project      = data.google_project.gcp_project.project_id
+}
+
+resource "google_project_iam_member" "secret_manager_access" {
+  project = data.google_project.gcp_project.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
+}
+
+resource "google_project_iam_member" "cloud_run_invoker" {
+  project = data.google_project.gcp_project.project_id
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
+}
+
+# -------------------------------------------------- #
 # HydroServer GCP Cloud Run Service NEG              #
 # -------------------------------------------------- #
 
-resource "google_compute_region_network_endpoint_group" "hydroserver_neg" {
-  name                  = "hydroserver-api-neg-${var.instance}"
-  region                = var.region
-  network_endpoint_type = "SERVERLESS"
+# resource "google_compute_region_network_endpoint_group" "hydroserver_neg" {
+#   name                  = "hydroserver-api-neg-${var.instance}"
+#   region                = var.region
+#   network_endpoint_type = "SERVERLESS"
 
-  cloud_run {
-    service = google_cloud_run_service.hydroserver_api.name
-  }
+#   cloud_run {
+#     service = google_cloud_run_service.hydroserver_api.name
+#   }
 
-  labels = {
-    var.label_key = local.label_value  
-  }
-}
+#   labels = {
+#     var.label_key = local.label_value  
+#   }
+# }
