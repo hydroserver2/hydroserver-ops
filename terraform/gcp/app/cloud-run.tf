a0edc19a5e91e2f7cfb15bb8c93af43a8e1108f7
Kenneth Lippold <kjlippold@outlook.com>
2024-10-30 11:34:10 -0700

    Update cloud-run.tf

--- terraform/gcp/app/cloud-run.tf
+++ terraform/gcp/app/cloud-run.tf
@@ -24,7 +24,7 @@ resource "google_cloud_run_v2_service" "hydroserver_api" {
         name  = "DATABASE_URL"
         value_source {
           secret_key_ref {
-            secret = "hydroserver-db-connection-${var.instance}"
+            secret = "hydroserver-database-url-${var.instance}"
             version  = "latest"
           }
         }
@@ -34,11 +34,87 @@ resource "google_cloud_run_v2_service" "hydroserver_api" {
         name  = "SECRET_KEY"
         value_source {
           secret_key_ref {
-            secret = "hydroserver-api-secret-key-${var.instance}"
-            version  = "latest"
+            secret  = "hydroserver-api-secret-key-${var.instance}"
+            version = "latest"
           }
         }
       }
+    }
+
+    env {
+      name  = "SMTP_URL"
+      value_source {
+        secret_key_ref {
+          secret  = google_secret_manager_secret.hydroserver_smtp_url.id
+          version = "latest"
+        }
+      }
+    }
+
+    env {
+      name  = "OAUTH_GOOGLE"
+      value_source {
+        secret_key_ref {
+          secret  = google_secret_manager_secret.hydroserver_oauth_google.id
+          version = "latest"
+        }
+      }
+    }
+
+    env {
+      name  = "OAUTH_ORCID"
+      value_source {
+        secret_key_ref {
+          secret  = google_secret_manager_secret.hydroserver_oauth_orcid.id
+          version = "latest"
+        }
+      }
+    }
+
+
+    env {
+      name  = "OAUTH_HYDROSHARE"
+      value_source {
+        secret_key_ref {
+          secret  = google_secret_manager_secret.hydroserver_oauth_hydroshare.id
+          version = "latest"
+        }
+      }
+    }
+
+    env {
+      name  = "DEPLOYED"
+      value = "True"
+    }
+
+    env {
+      name  = "DEPLOYMENT_BACKEND"
+      value = "gcp"
+    }
+
+    env {
+      name  = "STORAGE_BUCKET"
+      value = ""
+    }
+
+    env {
+      name  = "ACCOUNTS_EMAIL"
+      value = var.accounts_email
+    }
+
+    env {
+      name  = "PROXY_BASE_URL"
+      value = var.proxy_base_url
+    }
+
+    env {
+      name  = "ALLOWED_HOSTS"
+      value = var.allowed_hosts
+    }
+
+    env {
+      name  = "DEBUG"
+      value = var.debug
     }
 
     service_account = google_service_account.cloud_run_service_account.email
@@ -52,6 +128,74 @@ resource "google_cloud_run_v2_service" "hydroserver_api" {
       "${var.label_key}" = local.label_value
     }
   }
+}
+
+# -------------------------------------------------- #
+# HydroServer GCP Cloud Run Environment Secrets      #
+# -------------------------------------------------- #
+
+resource "google_secret_manager_secret" "hydroserver_smtp_url" {
+  secret_id = "hydroserver-smtp-url-${var.instance}"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "hydroserver_smtp_url_version" {
+  secret      = google_secret_manager_secret.hydroserver_smtp_url.id
+  secret_data = var.smtp_url
+}
+
+resource "google_secret_manager_secret" "hydroserver_oauth_google" {
+  secret_id = "hydroserver-oauth-google-${var.instance}"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "hydroserver_oauth_google_version" {
+  secret      = google_secret_manager_secret.hydroserver_oauth_google.id
+  secret_data = var.oauth_google
+}
+
+resource "google_secret_manager_secret" "hydroserver_oauth_hydroshare" {
+  secret_id = "hydroserver-oauth-hydroshare-${var.instance}"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "hydroserver_oauth_hydroshare_version" {
+  secret      = google_secret_manager_secret.hydroserver_oauth_hydroshare.id
+  secret_data = var.oauth_hydroshare
+}
+
+resource "google_secret_manager_secret" "hydroserver_oauth_orcid" {
+  secret_id = "hydroserver-oauth-orcid-${var.instance}"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "hydroserver_oauth_orcid_version" {
+  secret      = google_secret_manager_secret.hydroserver_oauth_orcid.id
+  secret_data = var.oauth_orcid
 }
 
 # -------------------------------------------------- #
