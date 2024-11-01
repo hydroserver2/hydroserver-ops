64f5880e49dadbd93c7b5acaabe31c85dd703f73
Kenneth Lippold <kjlippold@outlook.com>
2024-11-01 15:42:33 -0700

    Update cloud-storage.tf

--- terraform/gcp/app/cloud-storage.tf
+++ terraform/gcp/app/cloud-storage.tf
@@ -10,3 +10,21 @@ resource "google_storage_bucket" "hydroserver_storage_bucket" {
   uniform_bucket_level_access = true
   public_access_prevention = "enforced"
 }
+
+# -------------------------------------------------- #
+# HydroServer Data Management App Bucket             #
+# -------------------------------------------------- #
+
+resource "google_storage_bucket" "hydroserver_data_mgmt_app_bucket" {
+  name          = "hydroserver-data-mgmt-app-${var.instance}-${var.project_id}"
+  location      = var.region
+  project       = var.project_id
+  force_destroy = true
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  website {
+    main_page_suffix = "index.html"
+  }
+}
+
