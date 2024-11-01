cc2d8c0b0208feaa43a1785b3a2630cd94eb7ddc
Kenneth Lippold <kjlippold@outlook.com>
2024-11-01 15:50:10 -0700

    Update cloud-cdn.tf

--- terraform/gcp/app/cloud-cdn.tf
+++ terraform/gcp/app/cloud-cdn.tf
@@ -12,19 +12,29 @@ resource "google_compute_backend_service" "cloud_run_backend" {
 }
 
 # -------------------------------------------------- #
+# Cloud CDN Backend Bucket - Web Content             #
+# -------------------------------------------------- #
+
+resource "google_compute_backend_bucket" "data_mgmt_bucket_backend" {
+  name       = "hydroserver-${var.instance}-data-mgmt-bucket"
+  bucket_name = google_storage_bucket.hydroserver_data_mgmt_app_bucket.name
+  enable_cdn  = true
+}
+
+# -------------------------------------------------- #
 # URL Map                                            #
 # -------------------------------------------------- #
 
 resource "google_compute_url_map" "cdn_url_map" {
   name            = "hydroserver-${var.instance}-cdn-url-map"
-  default_service = google_compute_backend_service.cloud_run_backend.id
+  default_service = google_compute_backend_bucket.data_mgmt_bucket_backend.id
   host_rule {
     hosts        = ["*"]
     path_matcher = "allpaths"
   }
   path_matcher {
     name            = "allpaths"
-    default_service = google_compute_backend_service.cloud_run_backend.self_link
+    default_service = google_compute_backend_bucket.data_mgmt_bucket_backend.self_link
     path_rule {
       paths   = ["/api/*", "/admin/*"]
       service = google_compute_backend_service.cloud_run_backend.self_link
