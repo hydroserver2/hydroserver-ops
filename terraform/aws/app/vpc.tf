395db0123cbe5b024f57f639733e3451882bfcf4
Kenneth Lippold <kjlippold@outlook.com>
2024-11-07 16:01:40 -0800

    Update vpc.tf

--- terraform/aws/app/vpc.tf
+++ terraform/aws/app/vpc.tf
@@ -13,24 +13,13 @@ resource "aws_vpc" "hydroserver_vpc" {
   }
 }
 
-resource "aws_subnet" "hydroserver_subnet" {
-  vpc_id            = aws_vpc.hydroserver_vpc.id
-  cidr_block        = "10.8.0.0/24"
-  availability_zone = "${var.region}a"
-
-  tags = {
-    Name             = "hydroserver-${var.instance}"
-    "${var.tag_key}" = var.tag_value
-  }
-}
-
 resource "aws_subnet" "hydroserver_private_subnet_a" {
   vpc_id            = aws_vpc.hydroserver_vpc.id
   cidr_block        = "10.8.0.0/24"
   availability_zone = "${var.region}a"
 
   tags = {
-    Name             = "hydroserver-private-${var.instance}-a"
+    name             = "hydroserver-private-${var.instance}-a"
     "${var.tag_key}" = var.tag_value
   }
 }
@@ -41,7 +30,7 @@ resource "aws_subnet" "hydroserver_private_subnet_b" {
   availability_zone = "${var.region}b"
 
   tags = {
-    Name             = "hydroserver-private-${var.instance}-b"
+    name             = "hydroserver-private-${var.instance}-b"
     "${var.tag_key}" = var.tag_value
   }
 }
