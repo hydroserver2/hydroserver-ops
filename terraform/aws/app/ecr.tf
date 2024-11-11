47568032a5e84481c9321d38b9eece933077da27
Kenneth Lippold <kjlippold@outlook.com>
2024-11-11 14:24:48 -0800

    Update ecr.tf

--- terraform/aws/app/ecr.tf
+++ terraform/aws/app/ecr.tf
@@ -23,33 +23,12 @@ resource "aws_ecr_repository" "hydroserver_api_repository" {
 # VPC Endpoints for ECR                              #
 # -------------------------------------------------- #
 
-data "aws_subnet" "hydroserver_subnet_a" {
-  filter {
-    name   = "tag:name"
-    values = ["hydroserver-private-${var.instance}-a"]
-  }
-}
-
-data "aws_subnet" "hydroserver_subnet_b" {
-  filter {
-    name   = "tag:name"
-    values = ["hydroserver-private-${var.instance}-b"]
-  }
-}
-
-data "aws_security_group" "hydroserver_sg" {
-  filter {
-    name   = "tag:name"
-    values = ["hydroserver-${var.instance}"]
-  }
-}
-
 resource "aws_vpc_endpoint" "ecr_api_endpoint" {
   vpc_id            = aws_vpc.hydroserver_vpc.id
   service_name      = "com.amazonaws.${var.region}.ecr.api"
   vpc_endpoint_type = "Interface"
-  subnet_ids        = [data.aws_subnet.hydroserver_subnet_a.id, data.aws_subnet.hydroserver_subnet_b.id]
-  security_group_ids = [data.aws_security_group.hydroserver_sg.id]
+  subnet_ids        = [aws_subnet.hydroserver_subnet_a.id, aws_subnet.hydroserver_subnet_b.id]
+  security_group_ids = [aws_security_group.hydroserver_sg.id]
 
   tags = {
     "${var.label_key}" = var.label_value
@@ -60,8 +39,8 @@ resource "aws_vpc_endpoint" "ecr_dkr_endpoint" {
   vpc_id            = aws_vpc.hydroserver_vpc.id
   service_name      = "com.amazonaws.${var.region}.ecr.dkr"  # Docker ECR endpoint
   vpc_endpoint_type = "Interface"
-  subnet_ids        = [data.aws_subnet.hydroserver_subnet_a.id, data.aws_subnet.hydroserver_subnet_b.id]
-  security_group_ids = [data.aws_security_group.hydroserver_sg.id]
+  subnet_ids        = [aws_subnet.hydroserver_subnet_a.id, aws_subnet.hydroserver_subnet_b.id]
+  security_group_ids = [aws_security_group.hydroserver_sg.id]
 
   tags = {
     "${var.label_key}" = var.label_value
