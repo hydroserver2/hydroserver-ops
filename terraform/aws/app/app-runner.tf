353bcc53656770ba4f4b367d800e26cdb0875a1b
Kenneth Lippold <kjlippold@outlook.com>
2024-11-13 09:53:08 -0800

    Update app-runner.tf

--- terraform/aws/app/app-runner.tf
+++ terraform/aws/app/app-runner.tf
@@ -81,7 +81,7 @@ resource "aws_iam_role" "app_runner_service_role" {
         Action    = "sts:AssumeRole"
         Effect    = "Allow"
         Principal = {
-          Service = "build.apprunner.amazonaws.com"
+          Service = "apprunner.amazonaws.com"
         }
       },
     ]
