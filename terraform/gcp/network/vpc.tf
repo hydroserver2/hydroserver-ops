539cc1c7dd66b772beb6207c7b0d23b049089444
Kenneth Lippold <kjlippold@outlook.com>
2024-12-12 18:16:10 -0800

    Update vpc.tf

--- terraform/gcp/network/vpc.tf
+++ terraform/gcp/network/vpc.tf
@@ -4,7 +4,7 @@
 
 resource "google_compute_network" "hydroserver_vpc_network" {
   name                    = "hydroserver-${var.instance}"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
 }
 
 resource "google_compute_global_address" "hydroserver_private_service_ip_range" {
@@ -21,40 +21,6 @@ resource "google_service_networking_connection" "hydroserver_private_service_con
   reserved_peering_ranges = [
     google_compute_global_address.hydroserver_private_service_ip_range.name
   ]
-}
-
-# -------------------------------------------------- #
-# Private Subnets for Database                       #
-# -------------------------------------------------- #
-
-resource "google_compute_subnetwork" "hydroserver_private_db_subnet_a" {
-  name          = "hydroserver-private-db-${var.instance}-a"
-  ip_cidr_range = "10.8.0.0/24"
-  region        = var.region
-  network       = google_compute_network.hydroserver_vpc_network.id
-  private_ip_google_access = true
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 1.0
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
-}
-
-# -------------------------------------------------- #
-# Private Subnets for Applications                   #
-# -------------------------------------------------- #
-
-resource "google_compute_subnetwork" "hydroserver_private_app_subnet_a" {
-  name          = "hydroserver-private-app-${var.instance}-a"
-  ip_cidr_range = "10.8.2.0/24"
-  region        = var.region
-  network       = google_compute_network.hydroserver_vpc_network.id
-  private_ip_google_access = true
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 1.0
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
 }
 
 # -------------------------------------------------- #
