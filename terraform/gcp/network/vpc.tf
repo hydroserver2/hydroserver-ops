eba18fd238f043698dc77aae5e9d9b9df23e1125
Kenneth Lippold <kjlippold@outlook.com>
2024-12-13 09:53:36 -0800

    Update vpc.tf

--- terraform/gcp/network/vpc.tf
+++ terraform/gcp/network/vpc.tf
@@ -27,28 +27,28 @@ resource "google_service_networking_connection" "hydroserver_private_service_con
 # Cloud Router for VPC Internet Access               #
 # -------------------------------------------------- #
 
-# resource "google_compute_router" "hydroserver_vpc_router" {
-#   name    = "hydroserver-vpc-router-${var.instance}"
-#   region  = var.region
-#   network = google_compute_network.hydroserver_vpc_network.id
-# }
+resource "google_compute_router" "hydroserver_vpc_router" {
+  name    = "hydroserver-vpc-router-${var.instance}"
+  region  = var.region
+  network = google_compute_network.hydroserver_vpc_network.id
+}
 
-# resource "google_compute_router_nat" "hydroserver_vpc_router_nat" {
-#   name                       = "hydroserver-vpc-router-nat-${var.instance}"
-#   router                     = google_compute_router.hydroserver_vpc_router.name
-#   region                     = var.region
-#   nat_ip_allocate_option     = "AUTO_ONLY"
-#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-# }
+resource "google_compute_router_nat" "hydroserver_vpc_router_nat" {
+  name                       = "hydroserver-vpc-router-nat-${var.instance}"
+  router                     = google_compute_router.hydroserver_vpc_router.name
+  region                     = var.region
+  nat_ip_allocate_option     = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
 
-# resource "google_compute_firewall" "hydroserver_vpc_allow_egress_internet" {
-#   name    = "hydroserver-vpc-allow-egress-internet-${var.instance}"
-#   network = google_compute_network.hydroserver_vpc_network.id
+resource "google_compute_firewall" "hydroserver_vpc_allow_egress_internet" {
+  name    = "hydroserver-vpc-allow-egress-internet-${var.instance}"
+  network = google_compute_network.hydroserver_vpc_network.id
 
-#   allow {
-#     protocol = "all"
-#   }
+  allow {
+    protocol = "all"
+  }
 
-#   direction = "EGRESS"
-#   destination_ranges = ["0.0.0.0/0"]
-# }
+  direction = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+}
