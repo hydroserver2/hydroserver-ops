d6dd5f668dd425d939e4c2d354bd32f8d340e823
Kenneth Lippold <kjlippold@outlook.com>
2024-12-17 17:37:08 -0800

    Update cloud-storage.tf

--- terraform/gcp/app/cloud-storage.tf
+++ terraform/gcp/app/cloud-storage.tf
@@ -7,14 +7,14 @@ resource "google_storage_bucket" "hydroserver_storage_bucket" {
   location      = var.region
   project       = var.project_id
   force_destroy = false
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
 }
 
-# resource "google_storage_bucket_iam_member" "storage_bucket_public_access" {
-#   bucket = google_storage_bucket.hydroserver_storage_bucket.name
-#   role   = "roles/storage.objectViewer"
-#   member = "allAuthenticatedUsers"
-# }
+resource "google_storage_bucket_iam_member" "storage_bucket_public_access" {
+  bucket = google_storage_bucket.hydroserver_storage_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
 
 # -------------------------------------------------- #
 # HydroServer Data Management App Bucket             #
@@ -25,18 +25,18 @@ resource "google_storage_bucket" "hydroserver_data_mgmt_app_bucket" {
   location      = var.region
   project       = var.project_id
   force_destroy = true
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
 
   website {
     main_page_suffix = "index.html"
   }
 }
 
-# resource "google_storage_bucket_iam_member" "data_mgmt_app_bucket_public_access" {
-#   bucket = google_storage_bucket.hydroserver_data_mgmt_app_bucket.name
-#   role   = "roles/storage.objectViewer"
-#   member = "allAuthenticatedUsers"
-# }
+resource "google_storage_bucket_iam_member" "data_mgmt_app_bucket_public_access" {
+  bucket = google_storage_bucket.hydroserver_data_mgmt_app_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
 
 # -------------------------------------------------- #
 # Placeholder index.html File                        #
