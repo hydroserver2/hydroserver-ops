a27b241a962140e31b4bdf8d7e76a22a1111a2a8
Ken Lippold <klippold@Kens-MBP-2.lan>
2025-01-28 14:30:16 -0800

    Cleaned up old workflows.

--- terraform/aws/x-forwarded-host.js
+++
@@ -1,9 +0,0 @@
-function handler(event) {
-    var request = event.request;
-    var headers = request.headers;
-    var host = request.headers.host.value;
-    // Set x-forwarded-host header equal to host header
-    headers['x-forwarded-host'] = {value: host};
-
-    return request;
-}
