ef7d2821d0c481fb42816d632c28424919002c58
Ken Lippold <klippold@Kens-MBP-2.lan>
2025-01-30 20:57:53 -0800

    Updated app-runner Terraform

--- terraform/aws/app-runner.tf
+++ terraform/aws/app-runner.tf
@@ -42,9 +42,10 @@ resource "aws_apprunner_service" "api" {
   }
 
   health_check_configuration {
-    protocol = "HTTP"
-    interval = 20
-    timeout  = 18
+    protocol = "TCP"
+    interval = 5
+    timeout  = 2
+    unhealthy_threshold = 2
   }
 
   network_configuration {
