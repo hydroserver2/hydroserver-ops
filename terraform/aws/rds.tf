cdf5da86fa5433923d7e7248bdebf3bab4de2316
Ken Lippold <klippold@Kens-MBP-2.lan>
2025-02-01 13:05:46 -0800

    Updated RDS Terraform.

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -80,33 +80,33 @@ resource "random_string" "rds_db_user_password_prefix" {
 }
 
 
-# ---------------------------------
-# RDS Security Group
-# ---------------------------------
+# # ---------------------------------
+# # RDS Security Group
+# # ---------------------------------
 
-resource "aws_security_group" "rds_sg" {
-  name        = "hydroserver-${var.instance}-rds-sg"
-  vpc_id      = aws_vpc.rds_vpc.id
+# resource "aws_security_group" "rds_sg" {
+#   name        = "hydroserver-${var.instance}-rds-sg"
+#   vpc_id      = aws_vpc.rds_vpc.id
 
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # TODO remove
-    # security_groups = [aws_security_group.app_runner_sg.id]
-  }
+#   ingress {
+#     from_port       = 5432
+#     to_port         = 5432
+#     protocol        = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # TODO remove
+#     # security_groups = [aws_security_group.app_runner_sg.id]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    "${var.tag_key}" = local.tag_value
-  }
-}
+#   tags = {
+#     "${var.tag_key}" = local.tag_value
+#   }
+# }
 
 
 # ---------------------------------
