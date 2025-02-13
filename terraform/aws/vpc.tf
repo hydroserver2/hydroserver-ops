c66fe1e3375e9a6f3763acd60185715ef32f4186
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-02-13 14:37:21 -0800

    AWS security enhancements

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -132,12 +132,12 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-resource "aws_route_table_association" "private_route_association" {
+resource "aws_route_table_association" "private_route_association_az1" {
   subnet_id      = aws_subnet.private_subnet_az1.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_route_table_association" "private_route_association" {
+resource "aws_route_table_association" "private_route_association_az2" {
   subnet_id      = aws_subnet.private_subnet_az2.id
   route_table_id = aws_route_table.private_route_table.id
 }
@@ -161,12 +161,12 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table_association" "public_route_association" {
+resource "aws_route_table_association" "public_route_association_az1" {
   subnet_id      = aws_subnet.public_subnet_az1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "public_route_association" {
+resource "aws_route_table_association" "public_route_association_az2" {
   subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_route_table.id
 }
