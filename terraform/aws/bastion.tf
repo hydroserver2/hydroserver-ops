95359ffdc98f6915c5d8c23d5b258f561705b15a
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-06-27 11:12:16 -0700

    Updated AWS bastion host permissions

--- terraform/aws/bastion.tf
+++ terraform/aws/bastion.tf
@@ -6,13 +6,14 @@ resource "aws_instance" "bastion" {
   count = var.database_url == "" ? 1 : 0
 
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.private_subnet_az1.id
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   iam_instance_profile        = aws_iam_instance_profile.bastion_profile.name
   associate_public_ip_address = false
 
   tags = {
+    Name = "hydroserver-${var.instance}-ssm-bastion"
     "${var.tag_key}" = local.tag_value
   }
 }
@@ -35,9 +36,9 @@ data "aws_ami" "amazon_linux" {
 # ---------------------------------
 
 resource "aws_security_group" "bastion_sg" {
+  name        = "hydroserver-${var.instance}-bastion-sg"
   count = var.database_url == "" ? 1 : 0
 
-  name        = "bastion-sg"
   description = "Security group for HydroServer SSM bastion"
   vpc_id      = aws_vpc.vpc.id
 
@@ -62,9 +63,8 @@ resource "aws_security_group" "bastion_sg" {
 # ---------------------------------
 
 resource "aws_iam_role" "bastion_role" {
+  name = "hydroserver-${var.instance}-bastion-ssm-role"
   count = var.database_url == "" ? 1 : 0
-
-  name = "bastion-ssm-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -80,16 +80,41 @@ resource "aws_iam_role" "bastion_role" {
   })
 }
 
+resource "aws_iam_policy" "bastion_rds_policy" {
+  name  = "hydroserver-${var.instance}-bastion-rds-access-policy"
+  count = var.database_url == "" ? 1 : 0
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "rds-db:connect"
+        Resource = "arn:aws:rds-db:${var.region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.rds_db_instance[0].id}/hsdbadmin"
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ssm_core" {
+  name       = "hydroserver-${var.instance}-bastion-ssm-policy-attachment"
   count = var.database_url == "" ? 1 : 0
 
   role       = aws_iam_role.bastion_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_policy_attachment" "bastion_rds_policy_attachment" {
+  name       = "hydroserver-${var.instance}-bastion-rds-access-policy-attachment"
+  count      = var.database_url == "" ? 1 : 0
+
+  policy_arn = aws_iam_policy.bastion_rds_policy[0].arn
+  roles      = [aws_iam_role.bastion_role.name]
+}
+
 resource "aws_iam_instance_profile" "bastion_profile" {
+  name = "hydroserver-${var.instance}-bastion-ssm-profile"
   count = var.database_url == "" ? 1 : 0
 
-  name = "bastion-ssm-profile"
-  role = aws_iam_role.bastion_role.name
+  roles = [aws_iam_role.bastion_role.name]
 }
