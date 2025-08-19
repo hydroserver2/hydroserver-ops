ce214a97863a94cc2e3e0101743dfa464498fc0c
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-08-19 14:28:45 -0700

    Updated AWS worker deployment

--- terraform/aws/fargate.tf
+++ terraform/aws/fargate.tf
@@ -155,3 +155,8 @@ resource "aws_iam_policy_attachment" "ecs_worker_execution_logs" {
   roles      = [aws_iam_role.ecs_worker_execution_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_task_ssm_policy_attachment" {
+  role       = aws_iam_role.ecs_worker_execution_role.name
+  policy_arn = aws_iam_policy.ecs_task_execution_ssm_policy.arn
+}
